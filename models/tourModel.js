e06eff00fc82520b135c701f7163e22e8c2b9aa9
Tharusha-Lekamge <tharushalekamge.19@cse.mrt.ac.lk>
2022-05-17 19:46:47 +0530

    Added Mongoose middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // mongoose01 - creating schema
 // a field can have a set of attributes
@@ -14,9 +15,70 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  // TO specify that we need virtual properties
+  // to appear in the schema
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  slug: {
+    type: String,
+  },
+});
+
+// Virtual properties
+/*
+ * not saved in the DB
+ * Mostly used to store derived properties
+ * have to state specifically that
+ * we need the virtual properties in the schema
+ */
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.durarion / 7;
+  // Used a normal function to get access to thi keyword
+  // To return an own parameter
+});
+
+// DOCUMENT MIDDLEWARE
+// pre runs before. 'save' runs before saving and creating
+// Middleware should have next
+tourSchema.pre('save', function (next) {
+  // 'this' is the currently saving document
+  // eslint-disable-next-line no-console
+  console.log(this);
+  // Creata a slug
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// QUERY MIDDLEWARE
+tourSchema.pre('query', function (next) {
+  console.log(this);
+  this.find({ secretTour: { $ne: true } });
+  // This is the query object. Not a document
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: {
+      secretTour: { $ne: true },
+    },
+  });
+  // Unshift adds to the beginnning of the array
+  console.log(this.pipeline());
+  // 'this' is an aggregate object
+  next();
 });
 
 // Mongoose02 - Creating the model
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
+
+// Middleware types -
+/**
+ * Document
+ * Query
+ * Aggregate
+ * model
+ */
